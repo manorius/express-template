e25f0c72be078097f4b5c72b1437c8284b82992d
Manolis <manorius@gmail.com>
2015-02-27 03:57:30 +0800

    express with html boilerplate

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+  
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+  
+// view engine setup
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+  
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname, 'public'));
+  
+// ROOT DOMAIN
+app.get('/', function(req, res){
+  // FOR SENDING A STRING
+  // res.send('Manolis says, Hello World!');
+  // FOR LOADING A HTML FILE
+  res.render('index.html');
+  // SENDING JSON
+  // res.json({ status:"ALL OK" });
+// REDIRECTING TO A NEW PAGE
+// res.redirect("/blog");
+// DOWNLOADING A FILE
+// res.download('/directory/file.ext');
+});
+  
+  
+// RECEIVEING AND SENDING DATA THROUGH WEBSOCKETS
+io.on('connection', function(socket){
+    socket.on('yellow-line', function(){
+      socket.emit('red-line',{"data":23143});
+    });
+})
+  
+// READING GET REQUEST AND RESPONDING
+app.get('/getReq', function(req, res){
+  console.log(req.query.q);
+  res.send("received");
+});
+  
+// SENDING WESOCKET DATA
+app.get('/send-data-over-socket',function(req, res){
+  
+res.send('SENDING DATA');
+  
+io.sockets.emit('red-line',{"data":99999});
+  
+});
+  
+module.exports = app;
+  
+server.listen(4567, function(){
+  var host = server.address().address
+   var port = server.address().port
+  
+  console.log('Example app listening at http://%s:%s', host, port)
+  
+}); 
---
+++ public/js/functions.js
@@ -0,0 +1,33 @@
+// Browser detection for when you get desparate. A measure of last resort.
+// http://rog.ie/post/9089341529/html5boilerplatejs
+
+// var b = document.documentElement;
+// b.setAttribute('data-useragent',  navigator.userAgent);
+// b.setAttribute('data-platform', navigator.platform);
+
+// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
+
+
+// remap jQuery to $
+(function($){
+
+	/* trigger when page is ready */
+	$(document).ready(function (){
+	
+	console.log("ready");
+	
+	});
+	
+	$(window).load(function() {
+		
+	console.log("on load");
+	});
+	
+	$(window).resize(function() {
+		
+	console.log("resizing");
+	});
+	
+	
+
+})(window.jQuery);
